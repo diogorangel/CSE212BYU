944000b1472ee3f408d5aae20234152c9bdf6d37
Diogo Rangel Dos Santos <diodam.rangel@gmail.com>
2025-11-12 00:40:58 -0300

    assignments updateds week03 done and fixed now

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System;
+
 /// <summary>
 /// Defines a maze using a dictionary. The dictionary is provided by the
 /// user when the Maze object is created. The dictionary will contain the
@@ -25,6 +28,29 @@ public class Maze
         _mazeMap = mazeMap;
     }
 
+    // Helper method to consolidate movement logic
+    private void Move(int index, int deltaX, int deltaY)
+    {
+        var currentCoord = new ValueTuple<int, int>(_currX, _currY);
+        
+        // Check if the current location is in the map and if movement is allowed
+        // If TryGetValue fails, it means we are trying to move outside the defined maze boundaries, which is a wall.
+        if (_mazeMap.TryGetValue(currentCoord, out var validMoves) && validMoves.Length > index)
+        {
+            if (validMoves[index])
+            {
+                // Move is allowed, update coordinates
+                _currX += deltaX;
+                _currY += deltaY;
+                return;
+            }
+        }
+        
+        // If we reach here, it means there is a wall (validMoves[index] was false) or 
+        // the coordinate was outside the map's definition (boundary wall).
+        throw new InvalidOperationException("Can't go that way!");
+    }
+
     // TODO Problem 4 - ADD YOUR CODE HERE
     /// <summary>
     /// Check to see if you can move left.  If you can, then move.  If you
@@ -32,7 +58,8 @@ public class Maze
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        // Index 0: left, Change: x-1
+        Move(0, -1, 0);
     }
 
     /// <summary>
@@ -41,7 +68,8 @@ public class Maze
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        // Index 1: right, Change: x+1
+        Move(1, 1, 0);
     }
 
     /// <summary>
@@ -50,7 +78,8 @@ public class Maze
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        // Index 2: up, Change: y-1
+        Move(2, 0, -1);
     }
 
     /// <summary>
@@ -59,7 +88,8 @@ public class Maze
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        // Index 3: down, Change: y+1
+        Move(3, 0, 1);
     }
 
     public string GetStatus()
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,4 +1,9 @@
 using System.Text.Json;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 public static class SetsAndMaps
 {
@@ -6,23 +11,32 @@ public static class SetsAndMaps
     /// The words parameter contains a list of two character 
     /// words (lower case, no duplicates). Using sets, find an O(n) 
     /// solution for returning all symmetric pairs of words.  
-    ///
-    /// For example, if words was: [am, at, ma, if, fi], we would return :
-    ///
-    /// ["am & ma", "if & fi"]
-    ///
-    /// The order of the array does not matter, nor does the order of the specific words in each string in the array.
-    /// at would not be returned because ta is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be returned.
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
         // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var wordSet = new HashSet<string>(words);
+        var symmetricPairs = new List<string>();
+
+        foreach (var word in words)
+        {
+            // Reverse the word
+            var reversedWord = $"{word[1]}{word[0]}";
+
+            // Check if the reversed word is in the set
+            if (word != reversedWord && wordSet.Contains(reversedWord))
+            {
+                // To avoid adding the pair twice (e.g., "ab&ba" and later "ba&ab"), 
+                // we only add the pair where the original word comes first alphabetically.
+                // FIX: Changed format to use '&' and remove quotes, matching test expectations.
+                if (string.Compare(word, reversedWord) < 0)
+                {
+                    symmetricPairs.Add($"{word}&{reversedWord}");
+                }
+            }
+        }
+        return symmetricPairs.ToArray();
     }
 
     /// <summary>
@@ -39,10 +53,35 @@ public static class SetsAndMaps
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename))
+        // Using a try-catch to be robust against missing file, as requested for Problem 2.
+        try
         {
-            var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            foreach (var line in File.ReadLines(filename))
+            {
+                var fields = line.Split(",");
+                // TODO Problem 2 - ADD YOUR CODE HERE
+                // The degree is in the 4th column (index 3)
+                if (fields.Length > 3)
+                {
+                    var degree = fields[3].Trim();
+                    if (degrees.ContainsKey(degree))
+                    {
+                        degrees[degree] += 1;
+                    }
+                    else
+                    {
+                        degrees[degree] = 1;
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Error: File not found at {filename}. Cannot summarize degrees.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
 
         return degrees;
@@ -52,41 +91,47 @@ public static class SetsAndMaps
     /// Determine if 'word1' and 'word2' are anagrams.  An anagram
     /// is when the same letters in a word are re-organized into a 
     /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
         // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        // Normalize: remove spaces and convert to lowercase
+        var cleanWord1 = word1.Replace(" ", "").ToLower();
+        var cleanWord2 = word2.Replace(" ", "").ToLower();
+
+        if (cleanWord1.Length != cleanWord2.Length)
+        {
+            return false;
+        }
+
+        // Use a dictionary to count letter frequencies
+        var charCounts = new Dictionary<char, int>();
+
+        // Count characters in word1
+        foreach (var c in cleanWord1)
+        {
+            charCounts[c] = charCounts.GetValueOrDefault(c) + 1;
+        }
+
+        // Decrement counts for characters in word2
+        foreach (var c in cleanWord2)
+        {
+            charCounts[c] = charCounts.GetValueOrDefault(c) - 1;
+        }
+
+        // Check if all counts are zero
+        return charCounts.Values.All(count => count == 0);
     }
 
     /// <summary>
     /// This function will read JSON (Javascript Object Notation) data from the 
     /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will return a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
     /// </summary>
     public static string[] EarthquakeDailySummary()
     {
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+        
+        // This problem requires System.Net.Http, System.IO, and System.Text.Json
         using var client = new HttpClient();
         using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
         using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
@@ -94,13 +139,38 @@ public static class SetsAndMaps
         var json = reader.ReadToEnd();
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
+        // Deserializes using the custom classes defined below.
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
         // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        var summary = new List<string>();
+        if (featureCollection?.features != null)
+        {
+            foreach (var feature in featureCollection.features)
+            {
+                // Format: "place - Mag magnitude"
+                summary.Add($"{feature.properties.place} - Mag {feature.properties.mag}");
+            }
+        }
+        return summary.ToArray();
+    }
+
+    // --- Supporting Classes for Problem 5 JSON Deserialization ---
+    public class FeatureCollection
+    {
+        public List<Feature> features { get; set; }
+    }
+
+    public class Feature
+    {
+        public Properties properties { get; set; }
+    }
+
+    public class Properties
+    {
+        public double mag { get; set; }
+        public string place { get; set; }
     }
 }
